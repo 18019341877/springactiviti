4ac8776488ac8e936fb376a244d3e1817e632216
chb <abc837575234@126.com>
2020-04-08 21:25:44 +0800

    取消ssl

--- src/main/java/com/springactiviti/SpringActivitiApplication.java
+++ src/main/java/com/springactiviti/SpringActivitiApplication.java
@@ -1,5 +1,6 @@
 package com.springactiviti;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  **/
 @SpringBootApplication
 @ComponentScan({"com.springactiviti","org.activiti"})
+@MapperScan("com.springactiviti.domain.dao")
 public class SpringActivitiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringActivitiApplication.class, args);
